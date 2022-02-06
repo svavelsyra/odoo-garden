1cd93d23118527f96b6997b940f95d36ac55b50e
svavelsyra <68859611+svavelsyra@users.noreply.github.com>
2022-02-06 12:59:36 +0100

    Adding framework
    Manifest
    Basic view

---
+++ odoo-garden/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'GardenTrack',
+    'version': '14.0.1',
+    'category': 'other',
+    'license': 'AGPL-3',
+    'summary': 'Keep track of your garden',
+    'description': """Keeps track of your garden(s) and
+                      their flower beds and which flowers you have planted where.""",
+    'author': 'Svavelsyra',
+    'website': 'http://www.h2so4.se',
+    'depends': ['base'],
+    'data': ["views/views.xml"],
+    'qweb': [],
+    'auto_install': False,
+}
