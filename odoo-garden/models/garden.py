7ff1ec86b39ee53c84bd362cfa705408c4e3b0ae
svavelsyra <68859611+svavelsyra@users.noreply.github.com>
2022-03-13 15:59:43 +0100

    work in progress

--- odoo-garden/models/garden.py
+++ odoo-garden/models/garden.py
@@ -18,19 +18,37 @@ class Plant(models.Model):
     _name = 'plant'
 
     latin_name = fields.Char(string='Latin Name')
-    local_name = fields.Char(string='Local Name')
+    local_name = fields.Char(string='Local Name', translate=True)
     perennial = fields.Boolean(string='Perennial')
-    pregrow = fields.Many2many(string='Pregrow', comodel_name='month')
-    # plant_period = fields.Many2many(string='Plant Period', comodel_name='garden.month')
-    # flower_period = fields.Many2many(string='Flower Period', comodel_name='garden.month')
-    # zone = fields.Many2many(string='Growth Zone', comodel_name='garden.zone')
-    # plantbed_ids = fields.Many2many(string='Plant Beds', comodel_name='garden.plantbed')
-    main_image = fields.Binary()
-    images = fields.Binary()
-    description = fields.Char(string='Description')
+    #pregrow = fields.Many2many(string='Pregrow', comodel_name='pregrow_period_tag')
+    plant_period = fields.Many2many(string='Plant Period', comodel_name='plant_period_tag')
+    flower_period = fields.Many2many(string='Flower Period', comodel_name='flower_period_tag')
+    zone = fields.Many2many(string='Growth Zone', comodel_name='garden_zone')
+    plantbed_ids = fields.Many2many(string='Plant Beds', comodel_name='plantbed')
+    image = fields.Binary()
+    description = fields.Char(string='Description', translate=True)
     external_link = fields.Char(string='Link')
 
-class Month(models.Model):
-    _name = 'month'
 
+class Pregrow_Period_Tag(models.Model):
+    _name = 'pregrow_period_tag'
     name = fields.Char(string='Month Name')
+    number = fields.Integer(string='Month number')
+    
+
+class Plant_Period_Tag(models.Model):
+    _name = 'plant_period_tag'
+    name = fields.Char(string='Month Name')
+    number = fields.Integer(string='Month number')
+
+
+class Flower_Period_Tag(models.Model):
+    _name = 'flower_period_tag'
+    name = fields.Char(string='Month Name')
+    number = fields.Integer(string='Month number')
+    
+
+class Zone(models.Model):
+    _name = 'garden_zone'
+
+    name = fields.Char(string='Zone')
